52475dec34bd84934197cd526efe4cba15b20059
takuma <suzukichoco0227@yahoo.co.jp>
2022-06-18 19:08:22 +0900

    updated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $username = $_SESSION['name'];
-if (isset($_SESSION['id'])) {//ログインしているとき
+if (isset($_SESSION['userid'])) {//ログインしているとき
     $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
     $link = '<a href="logout.php">ログアウト</a>';
 } else {//ログインしていない時
