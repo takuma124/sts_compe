26e067d3e1234e3a646884bf37113a1dbca3c8b8
takuma <suzukichoco0227@yahoo.co.jp>
2022-06-18 10:36:11 +0900

    updated

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 //フォームからの値をそれぞれ変数に代入
 $name = $_POST['name'];
-$id = $_POST['userid'];
+$userid = $_POST['userid'];
 $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 $dsn = "mysql:host=localhost;dbname=stscompe;";
 $username = 'test';
@@ -14,21 +14,19 @@ try {
     $msg = $e->getMessage();
 }
 
-//フォームに入力されたmailがすでに登録されていないかチェック
 $sql = "SELECT * FROM users WHERE userid = :userid";
 $stmt = $dbh->prepare($sql);
-$stmt->bindValue(':userid', $id);
+$stmt->bindValue(':userid', $userid);
 $stmt->execute();
 $member = $stmt->fetch();
 if ($member['userid'] === $id) {
     $msg = '同じIDが存在します。';
     $link = '<a href="signup.php">戻る</a>';
 } else {
-    //登録されていなければinsert 
-    $sql = "INSERT INTO users(name, userid, pass) VALUES (:name, :useid, :pass)";
+    $sql = "INSERT INTO users(name, userid, pass) VALUES (:name, :userid, :pass)";
     $stmt = $dbh->prepare($sql);
     $stmt->bindValue(':name', $name);
-    $stmt->bindValue(':userid', $id);
+    $stmt->bindValue(':userid', $userid);
     $stmt->bindValue(':pass', $pass);
     $stmt->execute();
     $msg = '会員登録が完了しました';
@@ -36,5 +34,5 @@ if ($member['userid'] === $id) {
 }
 ?>
 
-<h1><?php echo $msg; ?></h1><!--メッセージの出力-->
+<h1><?php echo $msg; ?></h1>
 <?php echo $link; ?>
