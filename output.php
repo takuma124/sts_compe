32730eea241630e50d29e709c0077d0f052df2fb
takuma <suzukichoco0227@yahoo.co.jp>
2022-06-19 07:33:24 +0900

    updated

--- output.php
+++ output.php
@@ -3,18 +3,31 @@
 	<title>output.php</title>
 </head>
 <body>
-<!-- <?php
-	$name = $_POST['name'];
-	$code = $_POST['code'];
+<?php
+	// $code = $_POST['code'];
+	$tempfile = $_FILES['code']['tmp_name'];
+	$filename = $_FILES['code']['name'];
 	print("次のデータを受け取りました<br>");
-	print("名前:$name<br>");
-	print("filename:$code<br>");
-	$command = "/Users/suzukitakuma/.pyenv/shims/python ./$code";
-	exec($command, $output, $state);
-	foreach ($output as $out) {
-		print_r($out . '<br>');
+	// print("名前:$code<br>");
+	print("filename:$filename<br>");
+	// $command = "/Users/suzukitakuma/.pyenv/shims/python ./$filename";
+	// exec($command, $output, $state);
+	// foreach ($output as $out) {
+		// print_r($out . '<br>');
+	// }
+	// print("state: $state<br>");
+
+	$path = './program/';
+	// print(is_uploaded_file($_FILES['code']['tmp_name']));
+	if(is_uploaded_file($tempfile)){
+		if(move_uploaded_file($tempfile, $path.$filename)){
+			echo "$filename をアップロードしました。";
+		}else{
+			echo "ファイルをアップロードできませんでした。";
+		}
+	}else{
+		echo "ファイルが選択されていません。";
 	}
-	print("state: $state<br>");
-?> -->
+?>
 </body>
 </html>
---
+++ up.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>テストサイト</title>
+</head>
+<body>
+入力フォームです,
+名前を入力してみましょう。
+<form action="output.php" method="post" enctype="multipart/form-data">
+	<input type="file" name="code" accept=".py">
+	<input type="submit" value="アップロード">
+</form>
+</body>
+</html>
