fb66620bae24c459520934538ce0ab43d827aa45
takuma <suzukichoco0227@yahoo.co.jp>
2022-06-19 14:59:02 +0900

    updated

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
 $username = $_SESSION['name'];
+$board = '';
+$up = '';
 if (isset($_SESSION['userid'])) {//ログインしているとき
     $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
     $link = '<a href="logout.php">ログアウト</a>';
+    $board = '<a href="board.php">リーダボード</a>';
+    $up = '<a href="up.php">提出はこちら</a>';
 } else {//ログインしていない時
     $msg = 'ログインしていません';
     $link = '<a href="login.php">ログイン</a>';
@@ -11,3 +15,6 @@ if (isset($_SESSION['userid'])) {//ログインしているとき
 ?>
 <h1><?php echo $msg; ?></h1>
 <?php echo $link; ?>
+
+<?php echo $board; ?>
+<?php echo $up; ?>
